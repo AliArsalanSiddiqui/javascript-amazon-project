f9f7c3687b5d4dad4b929072d8882282e0f02a13
aliarsalan <ali.arsalan1134@gmail.com>
2025-08-07 22:12:59 +0500

    made counter for cart

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -77,6 +77,13 @@ document.querySelectorAll('.js-addtocart').forEach((button)=>{
         quantity: 1
         });
       };
+
+      let totalQuantity = 0;
+
+      cart.forEach((item)=>{
+         totalQuantity += item.quantity;
+      })
+      document.querySelector('.js-cart-quantity').innerHTML = totalQuantity;
        console.log(cart);
     });
 });
