5d507fc91391a348c77e6d0a1fe4e70a807e6437
aliarsalan <ali.arsalan1134@gmail.com>
2025-08-09 21:49:28 +0500

    delete items from html

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((cartItem, index) => {
     }
   })
 
-    checkoutHTML += `<div class="cart-item-container">
+    checkoutHTML += `
+         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -100,7 +101,8 @@ document.querySelectorAll('.js-delete').forEach((button) =>{
 button.addEventListener('click', () =>{
   const productId = button.dataset.productId;
   removeFromCart(productId);
-  console.log(cart);
+  const container = document.querySelector(`.js-cart-item-container-${productId}`)
+  container.remove();
  });
 })
 
